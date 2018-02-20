cee8f175c7eb9430223d1e664b32281e96f3e942
CharlieRa <carlos.ramart@gmail.com>
2018-02-19 21:00:44 -0300

    change checkbox for cards

--- src/components/ResolutionList.vue
+++ src/components/ResolutionList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container fluid grid-list-lg>
     <v-layout row align-center>
       <v-flex xs6 sm6>
         <p>Accomplished Resolutions:
@@ -10,32 +10,20 @@
             {{resolutions.filter(resolution => {return resolution.done === false}).length}}</p>
         </v-flex>
       </v-layout>
-      <v-layout column>
-        <v-card xs12 sm12 md6 >
-          <v-list two-line subheader>
-            <v-list-tile avatar v-for="resolution in resolutions" :key="resolution.id">
-              <v-list-tile-action>
-                <v-checkbox v-model="resolution.done"></v-checkbox>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title> {{resolution.title}} </v-list-tile-title>
-                <v-list-tile-sub-title> {{resolution.detail}} </v-list-tile-sub-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-                <v-btn icon ripple v-show="authenticate">
-                  <v-icon color="blue lighten-1">edit</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-              <v-list-tile-action>
-                <v-btn icon ripple v-show="authenticate" v-on:click="deleteTodo(resolution)">
-                  <v-icon color="red lighten-1">delete</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
-    </v-layout>
+         <v-layout row wrap>
+          <v-flex xs12 v-for="resolution in resolutions" :key="resolution.id">
+            <!-- <v-card color="blue-grey darken-2" class="white--text"> -->
+            <v-card class="white--text">
+              <v-card-title primary-title>
+                <div class="headline"> {{ resolution.title }} </div>
+                <div> {{ resolution.detail }} </div>
+              </v-card-title>
+              <v-card-actions>
+                <!-- <v-btn flat dark>Complete</v-btn> -->
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
   </v-container>
 </template>
 
