e562e0bb2113867d46a304a52b7795a7ebee949a
Carlos Ramart <carlos.ramart@gmail.com>
2018-04-18 21:45:08 -0300

    find resolutions by uid

--- src/components/ResolutionList.vue
+++ src/components/ResolutionList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid grid-list-lg>
-    <v-layout row align-center>
+    <!-- <v-layout row align-center>
       <v-flex xs6 sm3 offset-sm3>
         <p>Accomplished Resolutions:
           {{resolutions.filter(resolution => {return resolution.done === true}).length}}</p>
@@ -10,16 +10,16 @@
             {{resolutions.filter(resolution => {return resolution.done === false}).length}}
           </p>
         </v-flex>
+      </v-layout> -->
+      <v-layout row wrap>
+        <Resolution 
+        v-on:edit-resolution="editResolution"
+        v-on:complete-resolution="completeResolution"
+        v-on:remove-resolution="deleteResolution"
+        v-for="resolution in resolutions" 
+        v-bind:resolution="resolution" 
+        :key="resolution.id"></Resolution>
       </v-layout>
-         <v-layout row wrap>
-             <Resolution 
-              v-on:edit-resolution="editResolution"
-              v-on:complete-resolution="completeResolution"
-              v-on:remove-resolution="deleteResolution"
-              v-for="resolution in resolutions" 
-              v-bind:resolution="resolution" 
-              :key="resolution.id"></Resolution>
-        </v-layout>
   </v-container>
 </template>
 
@@ -30,47 +30,59 @@ import { config } from '../../config/firebase-config';
 
 const app = firebase.initializeApp(config);
 const db = app.database();
-const resolutionsReference = db.ref('resolutions').orderByChild('userId').equalTo('Leer un Libro cada Mes');
-
+// resolutionsReference = db.ref('resolutions').orderByChild('userId').
+// equalTo('4wtjre4Y2ccEndKaKRm3ULNAEWD3');
 export default {
   name: 'ResolutionList',
   data() {
     return {
       editDialog: false,
       isEditing: false,
+      user_ident: 'lala',
+      resolutions: null,
     };
+  },
+  mounted() {
+    console.log(this.userId);
+    db.ref('resolutions')
+      .orderByChild('userId')
+      .equalTo(this.userId)
+      .on('value', (snapshot) => {
+        this.resolutions = snapshot.val();
+      });
   },
   props: ['auth', 'userId'],
   methods: {
     addResolution(newResolution) {
-      resolutionsReference.push(
+      this.resolutions.push(
         newResolution,
       );
       console.log(newResolution);
     },
     completeResolution(resolution) {
       console.log(this.userId);
-      resolutionsReference.child(resolution.res['.key']).update({
+      this.resolutions.child(resolution.res['.key']).update({
         done: resolution.done,
       });
     },
     editResolution(resolution) {
-      console.log(resolutionsReference.child(resolution.res['.key']).update({
+      console.log(this.resolutions.child(resolution.res['.key']).update({
         title: resolution.title,
         detail: resolution.detail,
       }));
     },
     deleteResolution(resolution) {
       console.log(resolution);
-      resolutionsReference.child(resolution['.key']).remove();
+      this.resolutions.child(resolution['.key']).remove();
     },
   },
   components: {
     Resolution,
   },
-  firebase: {
-    resolutions: resolutionsReference,
-  },
+  // firebase: {
+  //   resolutions: db.ref('resolutions').
+  // orderByChild('userId').equalTo('4wtjre4Y2ccEndKaKRm3ULNAEWD3'),
+  // },
 };
 </script>
 <style scoped>
